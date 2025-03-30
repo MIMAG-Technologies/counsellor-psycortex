79f56c957d57ceac608510c626d56bd703f10822
Aniket Raut <rautnaniket@gmail.com>
2025-03-30 10:37:46 +0530

    add Firebase authentication; implement ProtectedRoute and AuthContext for user management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,29 @@
-
+"use client"
+import { AuthProvider } from "@/context/AuthContext";
 import "./globals.css";
+import ProtectedRoute from "@/components/ProtectedRoute";
+import { usePathname } from "next/navigation";
+
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+    const pathname = usePathname();
   return (
-    
     <html lang="en">
       <head>
-        <title>Counselor  | Psycortex</title>
-        <meta
-          name="description"
-          content=""
-        />
+        <title>Counselor | Psycortex</title>
+        <meta name="description" content="" />
         <link rel="icon" href="https://psycortex.in/favicon.ico" />
       </head>
       <body>
-        {children}
+        <AuthProvider>
+          {pathname !== "/login" && <ProtectedRoute />}
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
