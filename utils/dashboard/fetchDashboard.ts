bb99032c7347c6d220df91e633a95d3251348507
Aniket Raut <rautnaniket@gmail.com>
2025-05-08 10:40:21 +0530

    fix: fixed build errors

--- utils/dashboard/fetchDashboard.ts
+++ utils/dashboard/fetchDashboard.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CounselorStatsResponse } from "@/app/dashboard/types";
+import { CounselorStatsResponse } from "@/app/(authenticated)/dashboard/types";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
